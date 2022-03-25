182211c063d4813745b25123523acb5d42d8c456
macdavid08 <99491939+macdavid08@users.noreply.github.com>
2022-03-25 22:31:57 +0800

    added comment

--- Blog/models.py
+++ Blog/models.py
@@ -39,3 +39,5 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day,self.slug])
+
+# hello fucking world
